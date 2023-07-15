7217fc8464356605d403fd375b16b80513ffe91e
Ankit <ankit.singh14@nagarro.com>
2023-07-15 21:04:22 +0530

    Weather App Files

---
+++ WeatherApp/src/app/common.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+
+  constructor(private http:HttpClient) { }
+
+  getWeatherData(city:string):Observable<any>{
+    var  headers={
+      'X-RapidAPI-Key': 'e05c1a89a3mshf708f7bfb4278b4p12adeajsndc1210356642',
+      'X-RapidAPI-Host': 'openweather43.p.rapidapi.com'
+    }
+    var q = city;
+    var appid = ['da0f9c8d90bde7e619c3ec47766a42f4'];
+    var units = 'standard';
+      return this.http.get('https://openweather43.p.rapidapi.com/weather',{params:{q,appid,units },headers:headers})
+  }
+}
